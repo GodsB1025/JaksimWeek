75493544f99f97d51e8c50a9f6e667f3933eb402
KKB <godsb1025@gmail.com>
2025-08-04 11:43:24 +0900

    폴더구조 추가

--- app/src/main/java/com/team/jaksimweek/ui/main/MainActivity.kt
+++ app/src/main/java/com/team/jaksimweek/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.team.jaksimweek
+package com.team.jaksimweek.ui.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.team.jaksimweek.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
